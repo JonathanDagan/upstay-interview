f49f94d956c47ec9e2e60e40bc181c0825bef28f
Jonathan <jonathandagan12@gmail.com>
2023-01-16 14:21:54 +0200

    inital setup

---
+++ tests/test_main.py
@@ -0,0 +1,10 @@
+# test the add function
+from main import add
+import unittest
+
+
+class TestMain(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(10, 5), 15)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
